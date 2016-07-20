c419fe18359377e1f4927fa7502636ee35d8bcf5
yoshitakas <yoshitakasuzuki.7.3@gmail.com>
2016-07-20 06:33:15 +0000

    good job

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,8 @@ class UsersController < ApplicationController
   def show #追加
   
     @user = User.find(params[:id])
-    @microposts = @user.microposts.order(created_at: :desc).paginate(page: params[:page])
-    
+    @microposts = @user.microposts.order(created_at: :desc)
+    @microposts = @microposts.paginate(page: params[:page])
   end 
   
   def new
